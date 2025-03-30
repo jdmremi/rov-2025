6e3f8df13c332468e20c719f1194e91046bf4993
Saul Chavez <39887774+schavezz004@users.noreply.github.com>
2025-03-30 08:45:25 -0700

    Partial GUI development w/ some buttons

---
+++ app/GUI/sliderTest.py
@@ -0,0 +1,46 @@
+# pythonprogramminglanguage.com
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
+                             QMenu, QPushButton, QRadioButton, QVBoxLayout, QWidget, QSlider)
+
+class Window(QWidget):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+
+        grid = QGridLayout()
+        grid.addWidget(self.createExampleGroup(), 0, 0)
+        grid.addWidget(self.createExampleGroup(), 1, 0)
+        grid.addWidget(self.createExampleGroup(), 0, 1)
+        grid.addWidget(self.createExampleGroup(), 1, 1)
+        self.setLayout(grid)
+
+        self.setWindowTitle("PyQt5 Sliders")
+        self.resize(400, 300)
+
+    def createExampleGroup(self):
+        groupBox = QGroupBox("Slider Example")
+
+        radio1 = QRadioButton("&Radio horizontal slider")
+
+        slider = QSlider(Qt.Horizontal)
+        slider.setFocusPolicy(Qt.StrongFocus)
+        slider.setTickPosition(QSlider.TicksBothSides)
+        slider.setTickInterval(10)
+        slider.setSingleStep(1)
+
+        radio1.setChecked(True)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(radio1)
+        vbox.addWidget(slider)
+        vbox.addStretch(1)
+        groupBox.setLayout(vbox)
+
+        return groupBox
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    clock = Window()
+    clock.show()
+    sys.exit(app.exec_())
